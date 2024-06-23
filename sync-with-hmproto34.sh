1fa0bfe9d29b9a02dfecdf540935c2caba2a5287
hmasdev <hmasuidev@gmail.com>
2024-06-23 16:51:42 +0900

    update sync script

--- sync-with-hmproto34.sh
+++ sync-with-hmproto34.sh
@@ -2,5 +2,5 @@
 git clone git@github.com:hmasdev/hmproto34.git ./hmproto34
 cp hmproto34/keymaps/default/keymap.c ./keymaps/default/keymap.c
 cp hmproto34/keymaps/default/keymap.c ./keymaps/vial/keymap.c
-cp -r hmproto34/keymaps/hmasdevmap/keymap.c ./keymaps/hmasdevmap/keymap.c
+cp -r hmproto34/keymaps/hmasdevmap ./keymaps/
 rm -rf hmproto34
