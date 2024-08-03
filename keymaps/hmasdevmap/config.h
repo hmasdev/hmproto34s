85bc2152ea195416be065a31d19994728e5b7e3e
hmasdev <hmasuidev@gmail.com>
2024-08-03 22:47:46 +0900

    feat: Add Vial keymap configuration file

--- keymaps/hmasdevmap/config.h
+++ keymaps/hmasdevmap/config.h
@@ -4,3 +4,8 @@
 
 #define TAP_CODE_DELAY 5
 #define COMBO_TERM 50
+
+#pragma once
+#define VIAL_KEYBOARD_UID {0xBC, 0x17, 0xE9, 0x5D, 0x00, 0x9A, 0x4C, 0xC8}
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 1 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 9 }
